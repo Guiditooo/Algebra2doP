61a53dd42f5e8ac7f86abac518bfbeb95ffc299f
Guido Tello <lpmaleinn@gmail.com>
2021-06-24 20:28:22 -0300

    Avance 24/06 20:30
    Agregadas las consignas para cada ejercicio.
    1) Se hace solo media circunferencia (ARREGLAR).

---
+++ Algebra 2do Parcial/Assets/Ejercicios/Consignas.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * 1)   Vec1 gira el vector al rededor de los ejes
+ * 
+ * 2)   Vec1 gira al rededor de los ejes.
+ *      Vec2 vertical (en el mismo sentido que el eje Y). Gira a 10 unidades de los ejes.
+ *      Vec3 gira a 10 unidades de altura y distancia de los ejes.
+ * 
+ * 3)   Vec1 gira entre el 10,0,0 y el 0,10,0. Usa como eje 45° de rotacion del eje Z.
+ *      Vec2 usa la punta del Vec1 como base y llega al 10,10,0.
+ *      Vec3 gira entre el 20,10,0 y el 10,20,0. Usa como eje 45° de rotacion del eje Z.
+ *      Vec4 usa la punta del Vec1 como base y llega al 20,20,0.
+*/
---
+++ Algebra 2do Parcial/Assets/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EjerciciosAlgebra
+{
+
+    public class Ejercicio1 : MonoBehaviour
+    {
+
+        public int presettedLenght = 0;
+        public float presettedAngle = 0;
+
+        VectorArrow arrow;
+
+        private void Start()
+        {
+            VectorDebugger.AddVector(Vector3.zero, Color.red, "0");
+            VectorDebugger.AddVector(new Vector3(presettedLenght, 0, 0), Color.red, "1");
+
+            arrow = new VectorArrow(1, 0.5f, 2);
+
+            VectorDebugger.EnableCoordinates();
+            VectorDebugger.EnableEditorView();
+            VectorDebugger.SetVectorArrow(arrow);
+
+            //target = new GameObject();
+            //target.transform.position = new Vector3(presettedLenght, 0, 0);
+
+        }
+        float a = 0;
+        private void Update()
+        {
+
+                
+
+            float x = (presettedLenght * Mathf.Tan(a) * Mathf.Sqrt(1 + Mathf.Pow(Mathf.Tan(a), 2))) / (1 + Mathf.Pow(Mathf.Tan(a), 2));
+            float z;
+
+            if (a <= 180)
+            {
+
+                z = Mathf.Sqrt(Mathf.Pow(presettedLenght, 2) - Mathf.Pow(x, 2));
+
+            }
+            else
+            {
+
+                z = -Mathf.Sqrt(Mathf.Pow(presettedLenght, 2) - Mathf.Pow(x, 2));
+
+                if (a == 360)
+                {
+                    a = 0;
+                }
+
+            }
+            
+
+            a += presettedAngle;
+
+            
+
+            VectorDebugger.UpdatePosition("1",new Vector3(x,0,z));
+
+        }
+
+    }
+
+}
