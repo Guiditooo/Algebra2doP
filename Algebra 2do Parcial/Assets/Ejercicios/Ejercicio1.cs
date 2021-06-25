13bafd26da319a52582e8476ccb7b634927b295e
Guido Tello <lpmaleinn@gmail.com>
2021-06-24 21:49:22 -0300

    Avance 24/06 21:50
    Ejercicio 1 terminado.

--- Algebra 2do Parcial/Assets/Ejercicios/Ejercicio1.cs
+++ Algebra 2do Parcial/Assets/Ejercicios/Ejercicio1.cs
@@ -11,28 +11,23 @@ namespace EjerciciosAlgebra
         public const int Lenght = 10;
         public float angle = 0;
 
-        Vector3 vec;
-
         private void Start()
         {
 
-            vec = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
-
-            VectorDebugger.AddVector(Vector3.zero, Color.red, "0");
             VectorDebugger.AddVector(new Vector3(Lenght, 0, 0), Color.red, "1");
 
-            VectorDebugger.EnableCoordinates();
             VectorDebugger.EnableEditorView();
+            VectorDebugger.EnableCoordinates(); 
 
         }
         private void Update()
         {
 
-            Vector3 act = transform.position;
+            Vector3 aux = new Vector3(0, angle, 0);
+            Vector3 act = VectorDebugger.GetVectorsPositions("1")[1];
+            Vector3 niu = Quaternion.Euler(aux)*act;
 
-            VectorDebugger.UpdatePosition("1", new Vector3(act.x * vec.x - act.y * vec.y, act.y * vec.x - act.x * vec.y, 0));
-
-            //transform.position = new Vector3(act.x * vec.x - act.y * vec.y, act.x * vec.y - act.y * vec.x, 0);
+            VectorDebugger.UpdatePosition("1", niu);
 
         }
 
